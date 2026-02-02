574d62f5ffcd5f8000977d9daf46f51d78853a47
Shakthi <shakthigbhss@gmail>
2026-02-02 22:02:08 +0530

    Added the user Model

---
+++ src/main/java/models/User.java
@@ -0,0 +1,39 @@
+package models;
+
+public class User {
+	
+	private String username;
+	private String password;
+	private int userid;
+	
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	
+	
+
+}
