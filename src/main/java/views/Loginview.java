76901e0beae955c213512331e2d31584605eb103
ShakthiMaheswaran <shakthigbhss@gmail.com>
2026-02-03 22:07:19 +0530

    added the icon(App Logo) for the application

--- src/main/java/views/Loginview.java
+++ src/main/java/views/Loginview.java
@@ -1,7 +1,9 @@
 package views;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
@@ -16,6 +18,8 @@ public class Loginview extends JFrame {
 		this.setSize(450,300);
 		this.setLayout(null);
 		this.setTitle("Login");
+//		this.setIconImage(new ImageIcon("/resources/cms_icon.png").getImage());
+		this.setIconImage(new ImageIcon(getClass().getResource("/cms_icon.png")).getImage());
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
@@ -23,13 +27,23 @@ public class Loginview extends JFrame {
 	
 	public void addGui() {
 		
+	
+		
 		this.btnLogin = new JButton("Login");
 		this.btnLogin.setBounds(175, 200, 100, 30);
 		this.add(this.btnLogin);
 		
+		JLabel lblUsername = new JLabel("Username:");
+		lblUsername.setBounds(75, 75, 100, 30);
+		this.add(lblUsername);
+		
 		this.txtUsername = new JTextField();
 		this.txtUsername.setBounds(150, 75, 150, 30);
 		this.add(this.txtUsername);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setBounds(75, 125, 100, 30);
+		this.add(lblPassword);
 		
 		this.txtPassword = new JPasswordField();
 		this.txtPassword.setBounds(150, 125, 150, 30);
