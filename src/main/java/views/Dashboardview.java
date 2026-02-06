bf4c03bef961e9edbd414d3149eb5cf6ad2d6924
ShakthiMaheswaran <shakthigbhss@gmail.com>
2026-02-06 09:11:55 +0530

    init the dashboard view

---
+++ src/main/java/views/Dashboardview.java
@@ -0,0 +1,19 @@
+package views;
+
+import javax.swing.JPanel;
+
+public class Dashboardview extends  JPanel {
+	
+	public Dashboardview() {
+		this.setSize(800, 600);
+		this.setLayout(null);
+		this.setVisible(true);
+		
+		initComponents();
+	}
+	
+	public void initComponents() {
+		
+	}
+
+}
